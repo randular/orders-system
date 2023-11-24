b368f47d40b467ecabb5a61aa089e2acee8538d9
randular <60974798+randular@users.noreply.github.com>
2023-11-24 23:37:58 +0530

    Add items view and controller

--- src/main/java/controller/DashboardFromController.java
+++ src/main/java/controller/DashboardFromController.java
@@ -3,11 +3,15 @@ package controller;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -31,11 +35,20 @@ public class DashboardFromController {
         timeline.play();
     }
 
-    public void onActionCustomerBtn(MouseEvent mouseEvent) {
+    public void onActionCustomerBtn(ActionEvent actionEvent) {
 
     }
 
-    public void onActionItemBtn(MouseEvent mouseEvent) {
+    public void onActionItemBtn(ActionEvent actionEvent) {
+        Stage stage = (Stage) paneDashboard.getScene().getWindow();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/ItemForm.fxml"))));
+            stage.setResizable(false);
+            stage.setTitle("Items");
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
