132fca8f16c57b08ccfea0b2f995dac07901736f
randular <60974798+randular@users.noreply.github.com>
2023-12-23 23:28:14 +0530

    Add the validation for fields to not be empty and qty check

--- src/main/java/controller/OrderFormController.java
+++ src/main/java/controller/OrderFormController.java
@@ -11,6 +11,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeTableColumn;
@@ -131,45 +132,58 @@ public class OrderFormController {
     @FXML
     void onActionAddToCart(ActionEvent event) {
         try {
-            double amount = itemModel.getItem(cmbItemCode.getValue().toString())
-                    .getUnitPrice() * Integer.parseInt(txtItemQty.getText());
-            JFXButton deleteBtn = new JFXButton("Delete");
-            deleteBtn.setStyle("-fx-background-color:#c91114;" +
-                    "-fx-font-weight: BOLD"
-            );
-            OrderTm orderTm = new OrderTm(
-                    cmbItemCode.getValue().toString(),
-                    txtItemDesc.getText(),
-                    Integer.parseInt(txtItemQty.getText()),
-                    amount,
-                    deleteBtn
-            );
-            deleteBtn.setOnAction(actionEvent -> {
-                tmList.remove(orderTm);
-                tblOrder.refresh();
-                tot-=orderTm.getAmt();
-                lblTotalAmount.setText(String.format("%.2f",tot));
-            });
-            boolean isExist = false;
-
-            tot+=amount;
+            int qtyOrdered = 0;
             for (OrderTm order:tmList) {
-                if (order.getCode().equals(orderTm.getCode())){
-                    order.setQty(order.getQty() + orderTm.getQty());
-                    order.setAmt(order.getAmt() + orderTm.getAmt());
-                    isExist = true;
+                if (order.getCode().equals(cmbItemCode.getValue().toString())){
+                    qtyOrdered += order.getQty();
                 }
             }
-            if (!isExist){
-                tmList.add(orderTm);
+            if (txtCustomerName.getText().isEmpty()|| txtItemDesc.getText().isEmpty() || txtItemQty.getText().isEmpty()){
+                new Alert(Alert.AlertType.ERROR,"Please enter Customer,Item and quantity details").show();
+            }else if (itemModel.getItem(cmbItemCode.getValue().toString()).getQty() < Integer.parseInt(txtItemQty.getText())+qtyOrdered){
+                new Alert(Alert.AlertType.ERROR,"Entered quantity is more than on hand quantity").show();
+            }else{
+                double amount = itemModel.getItem(cmbItemCode.getValue().toString())
+                        .getUnitPrice() * Integer.parseInt(txtItemQty.getText());
+                JFXButton deleteBtn = new JFXButton("Delete");
+                deleteBtn.setStyle("-fx-background-color:#c91114;" +
+                        "-fx-font-weight: BOLD"
+                );
+                OrderTm orderTm = new OrderTm(
+                        cmbItemCode.getValue().toString(),
+                        txtItemDesc.getText(),
+                        Integer.parseInt(txtItemQty.getText()),
+                        amount,
+                        deleteBtn
+                );
+                deleteBtn.setOnAction(actionEvent -> {
+                    tmList.remove(orderTm);
+                    tblOrder.refresh();
+                    tot-=orderTm.getAmt();
+                    lblTotalAmount.setText(String.format("%.2f",tot));
+                });
+                boolean isExist = false;
+
+                tot+=amount;
+                for (OrderTm order:tmList) {
+                    if (order.getCode().equals(orderTm.getCode())){
+                        order.setQty(order.getQty() + orderTm.getQty());
+                        order.setAmt(order.getAmt() + orderTm.getAmt());
+                        isExist = true;
+                    }
+                }
+                if (!isExist){
+                    tmList.add(orderTm);
+                }
+
+                TreeItem<OrderTm> treeItem = new RecursiveTreeItem<>(tmList,
+                        RecursiveTreeObject::getChildren);
+                tblOrder.setRoot(treeItem);
+                tblOrder.setShowRoot(false);
+
+                lblTotalAmount.setText(String.format("%.2f",tot));
             }
 
-            TreeItem<OrderTm> treeItem = new RecursiveTreeItem<>(tmList,
-                    RecursiveTreeObject::getChildren);
-            tblOrder.setRoot(treeItem);
-            tblOrder.setShowRoot(false);
-
-            lblTotalAmount.setText(String.format("%.2f",tot));
 
         } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
